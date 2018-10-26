72c43f47199468df4d413e0eba4c3d876d546b09
youssef maghzaz <youssefmaghzaz@gmail.com>
2018-10-26 20:32:49 +0200

    example for aws-amplify-custom-ui

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import SignIn from "./SignIn";
+import amplifyCustomUi from "aws-amplify-react-custom-ui";
+
+class HelloWorld extends Component {
+  render() {
+    console.log(this.props);
+    return <div> hello world </div>;
+  }
+}
+export default class App extends Component {
+  componentWillMount() {
+    amplifyCustomUi.setSignIn(SignIn);
+  }
+
+  render() {
+    const SecureHelloWrold = amplifyCustomUi.withAuthenticator(HelloWorld);
+    const d = {
+      hellp: "shhs",
+      history: "hello"
+    };
+    return <SecureHelloWrold {...d} />;
+  }
+}
